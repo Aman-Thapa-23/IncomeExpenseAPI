b76d178e14a2c9ef96636ab492d192d0654a08f7
Aman Thapa <79891651+Aman-Thapa-23@users.noreply.github.com>
2023-02-08 22:55:15 +0545

    Update url.py for loginview

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ app_name = 'authentication'
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-registration'),
     path('activate-account/', VerifyEmail.as_view(), name='activate-account'),
+    path('login/', LoginAPIView.as_view(), name='login'),
 ]
