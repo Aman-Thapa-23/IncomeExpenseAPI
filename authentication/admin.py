350f005ebb2e9f7d01e1a320255982dc40d72c0a
Aman Thapa <79891651+Aman-Thapa-23@users.noreply.github.com>
2023-02-08 20:40:51 +0545

    Create user register serializer

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 from .models import MyUser
+
+# Register your models here.
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'is_active', 'is_verified', 'is_staff', 'is_superuser', 'created_at', 'updated_at', 'last_login']
---
+++ authentication/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import MyUser
+from .utils import validate_password
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={'input_type': 'password'}, max_length=66, min_length=8, write_only=True)
+    confirm_password = serializers.CharField(style={'input_type': 'password'}, max_length=66, min_length=8, write_only=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['username', 'email', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'confirm_password': {'write_only': True}
+        }
+        read_only_fields = ['id']
+
+    def validate(self, data):
+        email = data.get('email')
+        username = data.get('username')
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if not email:
+            raise serializers.ValidationError({'email': 'Email is required'})
+
+        if not username.isalnum():
+            raise serializers.ValidationError({'username': 'Username should only contain letters and numbers'})
+
+        validate_password(password, confirm_password)
+
+        return data
+
+    def create(self, validated_data):
+        return MyUser.objects.create_user(username=validated_data['username'], email=validated_data['email'], password=validated_data['confirm_password'])
+
+
