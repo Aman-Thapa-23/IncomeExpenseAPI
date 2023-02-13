9c6bb7582abc8b2f4ed2cd1da446a0835e0f7a31
Aman Thapa <79891651+Aman-Thapa-23@users.noreply.github.com>
2023-02-13 11:43:00 +0545

    Create test case for login view

--- authentication/test/test_views.py
+++ authentication/test/test_views.py
@@ -68,3 +68,47 @@ class EmailVerficationTest(APITestCase):
         self.assertEqual(
             response.data, {'status': 'failed', 'message': 'Email was already verified.'})
 
+
+class LoginTestCase(APITestCase):
+    def setUp(self):
+        self.user = MyUser.objects.create_user(
+            email='test@test.com', username='testhahah', password='testuser')
+
+    def test_user_with_valid_credentials(self):
+        data = {
+            'email': 'test@test.com',
+            'password': 'testuser'
+        }
+        url = reverse('authenticate:login')
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'status': 'success',
+                                         'message': 'login successful',
+                                         'refresh': response.data['refresh'],
+                                         'access': response.data['access']})
+
+    def test_with_invalid_email(self):
+        data = {
+            'email': 'test@asddcasdc.com',
+            'password': 'testuser'
+        }
+        url = reverse('authenticate:login')
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'status': 'failed',
+            'message': "{'status': 'failed', 'message': 'enter valid email'}"
+        })
+
+    def test_with_invalid_credentials(self):
+        data = {
+            'email': 'test@test.com',
+            'password': 'testuserasdc'
+        }
+        url = reverse('authenticate:login')
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data, {
+            'status': 'failed',
+            'message': "{'status': 'failed', 'message': 'invalid credentials, Try Again.'}"
+        })
